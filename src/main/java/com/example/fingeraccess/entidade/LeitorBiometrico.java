e13b8213ee9f97a547d1ea89c27dcce1f4d6278f
viniciussouzacss <50721086+viniciussouzacss@users.noreply.github.com>
2020-04-13 18:57:09 -0300

    fix LeitorBiometrico entity

--- src/main/java/com/example/fingeraccess/entidade/LeitorBiometrico.java
+++ src/main/java/com/example/fingeraccess/entidade/LeitorBiometrico.java
@@ -2,16 +2,19 @@ package com.example.fingeraccess.entidade;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * LeitorBiometrico INSERT INTO LEITOR_BIOMETRICO VALUES( 1000, 125, 'Voktta',  'Principal', 'adm123')
  */
 
 @Entity
+@Table(name="LeitorBiometrico")
 public class LeitorBiometrico implements Serializable{
 
     /**
@@ -21,10 +24,19 @@ public class LeitorBiometrico implements Serializable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="ID", nullable=false, unique=true)
     private int id;
+
+    @Column(name="local", nullable=false, unique=false)
     private String local;
+
+    @Column(name="capacidade", nullable=false, unique=false)
     private int capacidade;
+
+    @Column(name="fabricante", nullable=false, unique=false)
     private String fabricante;
+
+    @Column(name="senha", nullable=false, unique=false)
     private String senha;
 
     public int getId() {
